02c0cdc285af20d56387538c9f4d853d23ca17c2
Miro <miroslav.koichev8@gmail.com>
2026-06-26 14:42:49 +0100

    patch(push) - Replace options with a PushFlags type.

--- src/commands/push.ts
+++ src/commands/push.ts
@@ -3,26 +3,37 @@ import { exists } from "node:fs/promises";
 import { mkdir } from "node:fs/promises";
 import { watch } from "node:fs";
 import { join } from "node:path";
+interface PushFlags {
+	watch?: boolean;
+	noConfig?: boolean;
+}
 /**
  * @todo - Functionality
  */
-export async function performPush(cwd: string) {
+export async function performPush(cwd: string, flags: PushFlags) {
 	const srcDir = join(cwd, "src");
 	const distDir = join(cwd, "dist");
 
-	/* Create dist if it doesn't exist. Ensures srcDir exists to ensure its being ran within a project */
-	if ((await exists(srcDir)) && !(await exists(distDir))) await mkdir(distDir, { recursive: true });
+	/* Ensure config file exists. Skipped when using --noConfig */
+	if ((await exists(join(cwd, "config.ts"))) && !flags.noConfig) outputAndExit(`Your project does not have a config.ts file. If you only want to push transpiled code, use '--noConfig'.`);
+
+	if ((await exists(srcDir)) && !(await exists(distDir)))
+		/* Create dist if it doesn't exist. Ensures srcDir exists to ensure its being ran within a project */
+		await mkdir(distDir, { recursive: true });
+
+	const tsFiles: Record<string, string> = {};
+	const entryFileContents = []; // The 'export * from file' of every file to be transpiled - Paramount for the bundler to bundle it.
 }
 
-export async function push(options: { watch?: boolean } = {}) {
+export async function push(options: PushFlags = {}) {
 	const cwd = process.cwd();
 	const srcDir = join(cwd, "src");
 
 	if (!(await exists(srcDir))) outputAndExit(`The source directory does not exist. Ensure that you're running this process from the root of the project.`);
 
-	await performPush(cwd);
+	await performPush(cwd, options);
 
-	if (options.watch) await watchDirectoryForChanges(cwd);
+	if (options?.watch) await watchDirectoryForChanges(cwd, options);
 
 	process.exit(0); // Push complete
 
@@ -34,7 +45,7 @@ export async function push(options: { watch?: boolean } = {}) {
  * Upon detecting a change, triggers a push of the project.
  * Runs continuously until the process exits.
  */
-async function watchDirectoryForChanges(rootDir: string) {
+async function watchDirectoryForChanges(rootDir: string, flags: PushFlags) {
 	console.log(`Watching for changes in the 'src' directory of the project`);
 	let isPushing = false;
 	let timeout: ReturnType<typeof setTimeout> | null = null;
@@ -51,7 +62,7 @@ async function watchDirectoryForChanges(rootDir: string) {
 			console.log(`\nChange detected${filename ? `: ${filename}` : ""}. Pushing...`);
 
 			try {
-				await performPush(rootDir);
+				await performPush(rootDir, flags);
 				console.log(`Push complete. Watching for changes...`);
 			} catch (error) {
 				console.error(`Push failed:`, error);
